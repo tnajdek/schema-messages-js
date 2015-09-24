972f38403fe6b6853181854e1d9730edfeb04f8c
Tom Najdek <tom@doppnet.com>
2015-09-25 00:29:25 +0100

    Export interface functions

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import MessageFactory from './js/message-factory.js';
+import {
+	unpackMessage,
+	unpackMessages,
+	packMessage,
+	packMessages
+} from '../src/js/interface.js';
 
-module.exports = MessageFactory;
-
+module.exports = {
+	'MessageFactory': MessageFactory,
+	'unpackMessage': unpackMessage,
+	'unpackMessages': unpackMessages,
+	'packMessage': packMessage,
+	'packMessages': packMessages
+};
